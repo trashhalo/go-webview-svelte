89d7f6424d003aced948ff64304e889fc68a7107
Stephen Solka <solka@hey.com>
2020-08-20 13:45:01 -0400

    go in a different direction with http

--- main.go
+++ main.go
@@ -1,36 +1,24 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
-	"mime"
 	"net/http"
 	"net/http/httptest"
-	"os"
-	"path"
 
 	"github.com/markbates/pkger"
 	"github.com/webview/webview"
 )
 
-func httpServer() *httptest.Server {
-	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		url := path.Join("/frontend/public", r.URL.Path)
-		file, err := pkger.Open(url)
-		if err != nil && errors.Is(err, os.ErrNotExist) {
-			http.NotFound(w, r)
-		} else if err != nil {
-			panic(err)
-		}
-		defer file.Close()
+type pkgerServer struct{}
 
-		w.Header().Set("Content-Type", mime.TypeByExtension(url))
-		_, err = io.Copy(w, file)
-		if err != nil {
-			panic(err)
-		}
-	}))
+func (p *pkgerServer) Open(name string) (http.File, error) {
+	return pkger.Open(name)
+}
+
+func httpServer() *httptest.Server {
+	return httptest.NewServer(
+		http.FileServer(
+			&pkgerServer{}))
 }
 
 func main() {
@@ -39,7 +27,7 @@ func main() {
 	srv := httpServer()
 	defer srv.Close()
 
-	url := fmt.Sprintf("%s/index.html", srv.URL)
+	url := fmt.Sprintf("%s/frontend/public/index.html", srv.URL)
 	debug := true
 	w := webview.New(debug)
 	defer w.Destroy()
